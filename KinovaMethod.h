3249e850bf2dd76a596141c8fa26452c0b50eb37
Ma Zhenjia <mazhenjia@hotmail.com>
2017-09-11 17:04:57 +0900

    try velocity control 9/11

--- KinovaMethod.h
+++ KinovaMethod.h
@@ -20,6 +20,12 @@ int (*MySetActiveDevice)(KinovaDevice);
 int (*MySendBasicTrajectory)(TrajectoryPoint);
 int (*MyGetCartesianCommand)(CartesianPosition &);
 int (*MyGetCartesianPosition)(CartesianPosition &);
+int (*MyGetAngularPosition)(AngularPosition &);
+int (*MyGetAngularCommand)(AngularPosition &);
+int (*MyGetAngularVelocity)(AngularPosition &);
+int (*MySetAngularControl)();
+int (*MySetCartesianControl)();
+int (*MyGetControlType)(int &);
 
 int LoadAPI(){
   cout << "Loading API" << endl;
@@ -36,19 +42,74 @@ int LoadAPI(){
   MySendBasicTrajectory = (int (*)(TrajectoryPoint)) dlsym(commandLayer_handle, "SendBasicTrajectory");
   MyGetCartesianCommand = (int (*)(CartesianPosition &)) dlsym(commandLayer_handle, "GetCartesianCommand");
   MyGetCartesianPosition = (int (*)(CartesianPosition &)) dlsym(commandLayer_handle, "GetCartesianPosition");
-
+  MyGetAngularPosition = (int (*)(AngularPosition &)) dlsym(commandLayer_handle, "GetAngularPosition");
+  MyGetAngularCommand = (int (*)(AngularPosition &)) dlsym(commandLayer_handle, "GetAngularCommand");
+  MyGetAngularVelocity = (int (*)(AngularPosition &)) dlsym(commandLayer_handle, "GetAngularVelocity");
+  MySetAngularControl = (int (*)()) dlsym(commandLayer_handle, "SetAngularControl");
+  MySetCartesianControl = (int (*)()) dlsym(commandLayer_handle, "SetCartesianControl");
+  MyGetControlType = (int (*)(int &)) dlsym(commandLayer_handle, "GetControlType");
+  
   if (MyInitAPI==NULL || MyCloseAPI==NULL || MyMoveHome==NULL
       || MyInitFingers==NULL
       || MyGetDevices==NULL || MySetActiveDevice==NULL
       || MySendBasicTrajectory==NULL
-      || MyGetCartesianCommand==NULL || MyGetCartesianPosition==NULL){
+      || MyGetCartesianCommand==NULL || MyGetCartesianPosition==NULL
+      || MyGetAngularPosition==NULL || MyGetAngularCommand==NULL
+      || MySetAngularControl==NULL || MySetCartesianControl==NULL
+      || MyGetControlType==NULL){
     cout << " *** ERROR During Loading *** " << endl;
     return 0;
   }
   else{
-    cout << "Loading Complete" << endl;
+    // cout << "Loading Complete" << endl;
     return 1;
   }
+}
+
+void SetAngularPoint(AngularInfo &pA,
+		     double a1, double a2, double a3,
+		     double a4, double a5, double a6){
+  pA.Actuator1 = a1;
+  pA.Actuator2 = a2;
+  pA.Actuator3 = a3;
+  pA.Actuator4 = a4;
+  pA.Actuator5 = a5;
+  pA.Actuator6 = a6;
+}
+
+void SetCartesianPoint(CartesianInfo &pC,
+		       double x, double y, double z,
+		       double tx, double ty, double tz){
+  pC.X = x;
+  pC.Y = y;
+  pC.Z = z;
+  pC.ThetaX = tx;
+  pC.ThetaY = ty;
+  pC.ThetaZ = tz;
+}
+
+AngularInfo AngularPlus(AngularInfo pA1, AngularInfo pA2){
+  AngularInfo res;
+  res.InitStruct();
+  res.Actuator1 = pA1.Actuator1 + pA2.Actuator1;
+  res.Actuator2 = pA1.Actuator2 + pA2.Actuator2;
+  res.Actuator3 = pA1.Actuator3 + pA2.Actuator3;
+  res.Actuator4 = pA1.Actuator4 + pA2.Actuator4;
+  res.Actuator5 = pA1.Actuator5 + pA2.Actuator5;
+  res.Actuator6 = pA1.Actuator6 + pA2.Actuator6;
+  return res;
+}
+
+AngularInfo AngularMinus(AngularInfo pA1, AngularInfo pA2){
+  AngularInfo res;
+  res.InitStruct();
+  res.Actuator1 = pA1.Actuator1 - pA2.Actuator1;
+  res.Actuator2 = pA1.Actuator2 - pA2.Actuator2;
+  res.Actuator3 = pA1.Actuator3 - pA2.Actuator3;
+  res.Actuator4 = pA1.Actuator4 - pA2.Actuator4;
+  res.Actuator5 = pA1.Actuator5 - pA2.Actuator5;
+  res.Actuator6 = pA1.Actuator6 - pA2.Actuator6;
+  return res;
 }
 
 #endif
--- main.cpp
+++ main.cpp
@@ -25,28 +25,31 @@ int Display(CartesianPosition pNow){
 	 X, Y, Z, ThetaX/PI*180, ThetaY/PI*180, ThetaZ/PI*180);
 }
 
-int MoveVelocity(TrajectoryPoint pSend, int loop){
-  CartesianPosition pNow;
-  
-  int nDisp = 20;
-  int iDisp = 0;
-  MyGetCartesianPosition(pNow);
-  Display(pNow);
-  cout << endl;
-  for (int i=0; i<loop; i++){
-    if (iDisp==nDisp){
-      MyGetCartesianPosition(pNow);
-      Display(pNow);
-      cout << endl;
-      iDisp = 0;
-    }
-    else{
-      iDisp++;
-    }
-    
-    MySendBasicTrajectory(pSend);
-    usleep(5000);
-  }
+int Display_A(AngularPosition pA){
+  float A1 = pA.Actuators.Actuator1;
+  float A2 = pA.Actuators.Actuator2;
+  float A3 = pA.Actuators.Actuator3;
+  float A4 = pA.Actuators.Actuator4;
+  float A5 = pA.Actuators.Actuator5;
+  float A6 = pA.Actuators.Actuator6;
+
+  printf("A1=%7.2f, A2=%7.2f, A3=%7.2f, A4=%7.2f, A5=%7.2f, A6=%7.2f",
+	 A1, A2, A3, A4, A5, A6);
+}
+
+double abs(double x){
+  return x>0.0 ? x : -x;
+}
+
+double MAXANGLE(AngularInfo pA){
+  double maxa = 0;
+  maxa = max(maxa, abs(pA.Actuator1));
+  maxa = max(maxa, abs(pA.Actuator2));
+  maxa = max(maxa, abs(pA.Actuator3));
+  maxa = max(maxa, abs(pA.Actuator4));
+  maxa = max(maxa, abs(pA.Actuator5));
+  maxa = max(maxa, abs(pA.Actuator6));
+  return maxa;
 }
 
 int main(){
@@ -56,6 +59,7 @@ int main(){
   pSend.InitStruct();
 
   CartesianPosition pNow;
+  pNow.InitStruct();
   
   if (LoadAPI()==1){
     result = MyInitAPI();
@@ -73,56 +77,158 @@ int main(){
 
     cout << "Move HOME" << endl;
     MyMoveHome();
+    MyInitFingers();
+    
+    MyGetCartesianPosition(pNow);
+    pSend.Position.Type = CARTESIAN_POSITION;
+    pSend.Position.CartesianPosition = pNow.Coordinates;
+    pSend.Position.Fingers.Finger1 = 6000.0;
+    pSend.Position.Fingers.Finger2 = 6000.0;
+    MySendBasicTrajectory(pSend);
+    usleep(500000);
 
+    AngularPosition pA;
+    pA.InitStruct();
+    MyGetAngularPosition(pA);
+    Display_A(pA);
+    cout << endl;
+
+    AngularInfo p1, p2;
+    SetAngularPoint(p1, 350.0, 223.0, 73.0, 350.0, 35.0, 160.0);
+    SetAngularPoint(p2, 383.0, 230.0, 94.0, 346.0, 52.5, 189.0);
+    
     cout << "Move to Point #1" << endl;
-    pSend.Position.Type = CARTESIAN_POSITION;
-    pSend.Position.CartesianPosition.X = 0.0;
-    pSend.Position.CartesianPosition.Y = -0.3;
-    pSend.Position.CartesianPosition.Z = 0.15;
-    pSend.Position.CartesianPosition.ThetaX = PI/180.0*(180.0);
-    pSend.Position.CartesianPosition.ThetaY = PI/180.0*(0.0);
-    pSend.Position.CartesianPosition.ThetaZ = PI/180.0*(180.0);
 
-    MySendBasicTrajectory(pSend);
-    for (int i=0; i<40; i++){
-      MyGetCartesianPosition(pNow);
-      Display(pNow);
+    // Set the system to Angular control
+    result = MySetAngularControl();
+
+    int cType;
+    MyGetControlType(cType);
+    if (cType == CONTROL_TYPE_CARTESIAN){
+      cout << "Control Type : CARTESIAN" << endl;
+    }
+    else if (cType == CONTROL_TYPE_ANGULAR){
+      cout << "Control Type : ANGULAR" << endl;
+    }
+    else{
+      cout << " *** ERROR in Control Type *** " << endl;
+    }
+    
+    // cout << result << endl;
+    
+    pSend.Position.Type = ANGULAR_POSITION;
+    pSend.Position.Actuators = p1;
+    pSend.Position.Fingers.Finger1 = 0.0;
+    pSend.Position.Fingers.Finger2 = 0.0;
+    
+    result = MySendBasicTrajectory(pSend);
+
+    for (int i=0; i<30; i++){
+      MyGetAngularPosition(pA);
+      Display_A(pA);
       cout << endl;
       usleep(100000);
     }
-  
+
+    MyGetCartesianPosition(pNow);
+    Display(pNow);
+    cout << endl;
+    
     cout << "Move to Point #2" << endl;
-    pSend.Position.Type = CARTESIAN_POSITION;
-    pSend.Position.CartesianPosition.X = 0.10;
-    pSend.Position.CartesianPosition.Y = -0.2;
-    pSend.Position.CartesianPosition.Z = 0.15;
-    pSend.Position.CartesianPosition.ThetaX = PI/180.0*(-180.0);
-    pSend.Position.CartesianPosition.ThetaY = PI/180.0*(0.0);
-    pSend.Position.CartesianPosition.ThetaZ = PI/180.0*(180.0);
 
-    MySendBasicTrajectory(pSend);
+    // Set the system to Cartesian control
+    result = MySetCartesianControl();
+
+    // cout << result << endl;
+    pSend.InitStruct();
+
+    AngularInfo v1;
+    v1 = AngularMinus(p2, p1);
+    double maxa, coe;
+    double maxw = 20.0;
+    maxa = MAXANGLE(v1);
+    coe = maxa/maxw;
+    v1.Actuator1 /= coe;
+    v1.Actuator2 /= coe;
+    v1.Actuator3 /= coe;
+    v1.Actuator4 /= coe;
+    v1.Actuator5 /= coe;
+    v1.Actuator6 /= coe;
+    
+    pSend.Position.Type = ANGULAR_VELOCITY;   
+    pSend.Position.Actuators = v1;
+    pSend.Position.Fingers.Finger1 = 0.0;
+    pSend.Position.Fingers.Finger2 = 0.0;
+
+    int nDis = 20;
+    int iDis = 0;
+    bool flag = true;
+
+    while (flag){
+      MyGetAngularPosition(pA);
+      if (++iDis==nDis){
+	Display_A(pA);
+	cout << endl;
+	iDis = 0;
+      }
+
+      AngularInfo dp;
+      dp = AngularMinus(p2, pA.Actuators);
+      maxa = MAXANGLE(dp);
+      if (maxa < 0.3){
+	flag = false;
+      }
+      else{
+	coe = maxa/maxw;
+	dp.Actuator1 /= coe;
+	dp.Actuator2 /= coe;
+	dp.Actuator3 /= coe;
+	dp.Actuator4 /= coe;
+	dp.Actuator5 /= coe;
+	dp.Actuator6 /= coe;
+
+	pSend.InitStruct();
+	pSend.Position.Type = ANGULAR_VELOCITY;
+	pSend.Position.Actuators = dp;
+	pSend.Position.Fingers.Finger1 = 0.0;
+	pSend.Position.Fingers.Finger2 = 0.0;
+	
+	result = MySendBasicTrajectory(pSend);
+      }
+      usleep(5000);
+    }
+    
+    /*
     for (int i=0; i<20; i++){
-      MyGetCartesianPosition(pNow);
-      Display(pNow);
+      MyGetAngularPosition(pA);
+      Display_A(pA);
+      cout << endl;
+
+      for (int j=0; j<100000/5000; j++){
+	result = MySendBasicTrajectory(pSend);
+	usleep(5000);
+      }
+    }
+    */
+    
+    /*
+    result = MySendBasicTrajectory(pSend);
+
+    for (int i=0; i<20; i++){
+      MyGetAngularPosition(pA);
+      Display_A(pA);
       cout << endl;
       usleep(100000);
     }
+    */
 
-    cout << "Move to Point #3" << endl;
-    pSend.Position.CartesianPosition.X = -0.15;
-    pSend.Position.CartesianPosition.Y = -0.35;
-    pSend.Position.CartesianPosition.Z = 0.15;
-    pSend.Position.CartesianPosition.ThetaX = PI/180.0*(-180.0);
-    pSend.Position.CartesianPosition.ThetaY = PI/180.0*(0.0);
-    pSend.Position.CartesianPosition.ThetaZ = PI/180.0*(180.0);
+    MyGetAngularPosition(pA);
+    Display_A(pA);
+    cout << endl;
 
-    MySendBasicTrajectory(pSend);
-    for (int i=0; i<20; i++){
-      MyGetCartesianPosition(pNow);
-      Display(pNow);
-      cout << endl;
-      usleep(100000);
-    }
+    MyGetCartesianPosition(pNow);
+    Display(pNow);
+    cout << endl;
 
   }
 
